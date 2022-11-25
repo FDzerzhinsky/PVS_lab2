fc22598f951f4b09f4e04f74f03e3160260dec64
FDzerzhinsky <93994427+FDzerzhinsky@users.noreply.github.com>
2022-11-25 19:20:48 +0500

    Причёсаны переменные, убраны лишние комменты (все)

--- qsort_parallel/qsort_parallel.cpp
+++ qsort_parallel/qsort_parallel.cpp
@@ -5,54 +5,31 @@
 #include <ctime>
 using namespace std;
 
-// Разделение по схеме Lomuto
 int partition(int a[], int start, int end)
 {
-    // Выбираем крайний правый элемент в качестве опорного элемента массива
-    int pivot = a[end];
+    int right = a[end];
+    int sIndex = start;
 
-    // элементы, меньшие точки поворота, будут перемещены влево от `pIndex`
-    // элементы больше, чем точка поворота, будут сдвинуты вправо от `pIndex`
-    // равные элементы могут идти в любом направлении
-    int pIndex = start;
-
-    // каждый раз, когда мы находим элемент, меньший или равный опорному, `pIndex`
-    // увеличивается, и этот элемент будет помещен перед опорной точкой.
     for (int i = start; i < end; i++)
     {
-        if (a[i] <= pivot)
+        if (a[i] <= right)
         {
-            swap(a[i], a[pIndex]);
-            pIndex++;
+            swap(a[i], a[sIndex]);
+            sIndex++;
         }
     }
-
-    // поменять местами `pIndex` с пивотом
-    swap(a[pIndex], a[end]);
-
-    // вернуть `pIndex` (индекс опорного элемента)
-    return pIndex;
+        swap(a[sIndex], a[end]);
+    return sIndex;
 }
 
-// Процедура быстрой сортировки
-void quicksort(int a[], int start, int end)
+void qsort(int a[], int start, int end)
 {
-    // базовое условие
-    if (start >= end) {
+    if (start >= end) 
         return;
-    }
-
-    // переставить элементы по оси
-    int pivot = partition(a, start, end);
-
-    // повторяем подмассив, содержащий элементы, меньшие опорной точки
-    quicksort(a, start, pivot - 1);
-
-    // повторяем подмассив, содержащий элементы, превышающие точку опоры
-    quicksort(a, pivot + 1, end);
+    int opor = partition(a, start, end);
+    qsort(a, start, opor - 1);
+    qsort(a, opor + 1, end);
 }
-
-// C++ реализация алгоритма быстрой сортировки
 
 int *arrback(int arr[], const int n)
 {
@@ -66,6 +43,7 @@ int *arrback(int arr[], const int n)
     }
     return second;
 }
+
 int main()
 {
     int * a, *b;
@@ -91,33 +69,15 @@ int main()
 #pragma omp parallel shared(a) num_threads(k)
             {
 #pragma omp single nowait            
-                    quicksort(b, 0, n - 1);
+                    qsort(b, 0, n - 1);
             }
             //eTime = omp_get_wtime();
             eTime = std::clock();
             dTime = eTime - sTime;
             cout << "\n";
             cout << "\n";
-            cout << "Time of algoritm execution: " << dTime << " sec." << " for number of threads: " <<k;
+            cout << "Time of algoritm execution: " << dTime << " mSec." << " for number of threads: " <<k;
         }
     }
-
-
-    
-
-
-    //печатаем отсортированный массив
-    /*
-    cout << "\n";
-    for (int i = 0; i < n; i++) {
-        cout << a[i] << " ";
-    }
-    cout << "\n";
-    for (int i = 0; i < n; i++) {
-        cout << b[i] << " ";
-    }
-    
-    */
-
     return 0;
 }
